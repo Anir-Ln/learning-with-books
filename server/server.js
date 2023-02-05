a6685be2e2d0d9091c8464618606353850fb2577
Anir-Ln <howmed4@gmail.com>
2023-02-05 01:44:09 +0100

    configure cors

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const phrasesRouter = require('./routes/phrases.routes')
 
 const PORT = 8888 || process.env.PORT
+const corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200,
+}
 
+
+app.use(cors(corsOptions))
 app.use(express.json())
 
 // root endpoint
