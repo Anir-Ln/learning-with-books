055aad423f4b8ea2f65295f005108b7470901e9c
Anir-Ln <howmed4@gmail.com>
2023-02-05 01:45:00 +0100

    resolve imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import BookReader from './BookReader'
-import Dashboard from './Dashboard'
+import BookReader from './pages/BookReader'
+import Dashboard from './pages/Dashboard'
 
 
 const App = () => {
